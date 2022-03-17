7b591aaba2ebb11fa56a6c591518705c366ce5f6
Joel Temiz <joel.temiz@unibw.de>
2022-03-17 10:10:59 +0100

    Updated Design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./sites/home/home.component";
 import {AboutComponent} from "./sites/about/about.component";
 import {OffersComponent} from "./sites/offers/offers.component";
 import {SheepInfoComponent} from "./sites/sheep-info/sheep-info.component";
-import { ServiceComponent } from './sites/service/service.component';
+import {ServiceComponent} from './sites/service/service.component';
 
 const routes: Routes = [
-//  { path: '', component: HomeComponent },
-//  { path: 'aboutus', component: AboutComponent},
- // { path: 'articles', component: OffersComponent},
-//  { path: 'info', component: SheepInfoComponent}
-  {path: '', component: ServiceComponent}
+  {path: '', component: HomeComponent},
+  {path: 'aboutus', component: AboutComponent},
+  {path: 'articles', component: OffersComponent},
+  {path: 'info', component: SheepInfoComponent}
+  //{path: '', component: ServiceComponent}
 ];
 
 @NgModule({
@@ -25,7 +25,6 @@ const routes: Routes = [
     RouterModule
   ]
 })
-
 
 
 export class AppRoutingModule {
